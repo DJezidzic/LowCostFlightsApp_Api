afbcd8a6f926ceb97cf7a95f2fd020b96ac92bae
Dominik Jezidžić <djezidzic@tvz.hr>
2022-08-15 00:08:32 +0200

    Add project files.

---
+++ LowCostFlightsApp-Api/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LowCostFlightsAppApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    }
+}
